00d5d43aad78258f72f164263e45fecadb50f602
SlayerOrnstein <HydroNav@protonmail.com>
2021-06-18 19:59:27 -0400

    style: clean up comments

--- lib/market_client.dart
+++ lib/market_client.dart
@@ -1,11 +1,9 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library market_client;
 
 export 'src/client.dart';
 export 'src/enums.dart';
 export 'src/http.dart';
 export 'src/models/item.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/models/item_order.dart';
+export 'src/models/item_set.dart';
+export 'src/models/market_user.dart';
