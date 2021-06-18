948138fd18cbe5bd95fe6c18cd303b29b8304003
SlayerOrnstein <HydroNav@protonmail.com>
2021-06-18 19:39:58 -0400

    chore: add test for what's been added so far

--- test/client_test.dart
+++ test/client_test.dart
@@ -4,9 +4,16 @@ import 'package:mocktail/mocktail.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
+import 'fixtures/fixture_reader.dart';
+
 class MockMarketHttpClient extends Mock implements MarketHttpClient {}
 
 void main() {
+  final itemOrderFixture = fixture('item_order.json');
+  final itemsFixture = fixture('items.json');
+  final itemFixture = fixture('item.json');
+  final userProfileFixture = fixture('user_profile.json');
+
   late MarketHttpClient client;
   late MarketClient api;
 
@@ -15,18 +22,29 @@ void main() {
     api = MarketClient(client: client);
   });
 
-  test('Test Market Item retrivial', () async {
-    when(() => client.get('/items')).thenAnswer(
-      (i) => Future.value(<String, dynamic>{'items': []}),
-    );
+  test('Test items retrivial', () async {
+    when(() => client.get('/items'))
+        .thenAnswer((_) => Future.value(itemsFixture));
 
-    expect(await api.getMarketItems(), <MarketItem>[]);
+    final items = await api.getMarketItems();
+
+    expect(
+      items.map<Map<String, dynamic>>((e) => e.toJson()).toList(),
+      List<Map<String, dynamic>>.from(itemsFixture['items'] as List<dynamic>),
+    );
   });
 
-  test('Test retrivial of a specfic item', () {});
-
-  test('Test Market item exceptions', () {
+  test('Test items exceptions', () {
     when(() => client.get('/items')).thenThrow(BadRequestException());
     expect(() => api.getMarketItems(), throwsA(isA<BadRequestException>()));
+  });
+
+  test('Test retrivial of a single item', () async {
+    when(() => client.get('/items/secura_dual_cestra'))
+        .thenAnswer((_) => Future.value(itemFixture));
+
+    final item = await api.getMarketItem('secura_dual_cestra');
+
+    expect(item.toJson(), itemFixture['item'] as Map<String, dynamic>);
   });
 }
---
+++ test/fixtures/fixture_reader.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+Map<String, dynamic> fixture(String name) {
+  String fixture;
+
+  try {
+    fixture = _file(p.join('test', 'fixtures', name));
+  } catch (err) {
+    fixture = _file(p.join('fixtures', name));
+  }
+
+  return (json.decode(fixture) as Map<String, dynamic>)['payload'];
+}
+
+String _file(String path) => File(path).readAsStringSync();
