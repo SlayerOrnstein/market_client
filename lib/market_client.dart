51ca107fa2badb1021e0a7f4f7619f96d6a3a843
SlayerOrnstein <HydroNav@protonmail.com>
2022-08-09 19:31:32 -0400

    fix: export entities

--- lib/market_client.dart
+++ lib/market_client.dart
@@ -1,6 +1,7 @@
 library market_client;
 
 export 'src/clients/clients.dart';
+export 'src/entities/entities.dart';
 export 'src/enums.dart';
 export 'src/http.dart';
 export 'src/models/models.dart';
